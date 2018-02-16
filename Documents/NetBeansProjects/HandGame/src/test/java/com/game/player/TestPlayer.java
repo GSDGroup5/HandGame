ad524ee2f6b3b0269b28f83fb415f795438d9749
osama <pc@192.168.1.218>
2018-02-16 08:33:18 +0100

    Creating TestPlayer Class and adding Method for creating Rock Object

---
+++ Documents/NetBeansProjects/HandGame/src/test/java/com/game/player/TestPlayer.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.game.player;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pc
+ */
+public class TestPlayer {
+    
+    public TestPlayer() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testGenerateRock() {
+        Player player = new Player();
+        HandGame rock = player.createRock();
+        assertTrue(rock instanceof Rock);
+    }
+}
