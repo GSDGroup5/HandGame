95847b270dcd358d9df86e305b7f12d266db14cc
osama <pc@192.168.1.218>
2018-02-16 08:42:13 +0100

    Adding Method for calling the factory method for Rock , Paper and Scissor

--- Documents/NetBeansProjects/HandGame/src/main/java/com/game/player/Player.java
+++ Documents/NetBeansProjects/HandGame/src/main/java/com/game/player/Player.java
@@ -5,6 +5,7 @@
  */
 package com.game.player;
 
+import com.game.factory.HandGameFactory;
 import com.gameproject.handgame.HandGame;
 import com.gameproject.handgame.Paper;
 import com.gameproject.handgame.Rock;
@@ -16,6 +17,8 @@ import com.gameproject.handgame.Scissor;
  */
 public class Player {
 
+    private final static HandGameFactory HANDGAME = new HandGameFactory();
+
     public HandGame createRock() {
         return new Rock();
     }
@@ -23,8 +26,19 @@ public class Player {
     public HandGame createPaper() {
         return new Paper();
     }
-    
+
     public HandGame createScissor() {
         return new Scissor();
+    }
+
+    /**
+     * Create one of the options i.e Rock , Scissor ,Paper
+     *
+     * @param handGame i.e Rock , Scissor ,Paper
+     * @return return object i.e Rock , Scissor ,Paper
+     * @throws Exception class not found other then Rock , Scissor ,Paper
+     */
+    public HandGame invokeHandGame(String handGame) throws Exception {
+        return HANDGAME.newHandGame(handGame);
     }
 }
