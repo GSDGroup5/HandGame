9d55768f596e8a7f8526e955dd4eac401d0156de
osama <pc@192.168.1.218>
2018-02-16 08:35:28 +0100

    Creating Player Class and adding Method for creating Rock Object

---
+++ Documents/NetBeansProjects/HandGame/src/main/java/com/game/player/Player.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.game.player;
+
+import com.gameproject.handgame.HandGame;
+import com.gameproject.handgame.Rock;
+
+/**
+ *
+ * @author pc
+ */
+public class Player {
+
+    public HandGame createRock() {
+        return new Rock();
+    }
+}
